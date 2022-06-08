6b4b592c3e27b0ec929b6b006606ea3311a66924
Jasonccsze <89509616+Jasonccsze@users.noreply.github.com>
2022-06-08 21:43:10 +0800

    Create extract_data_edu.sql

---
+++ extract_data_edu.sql
@@ -0,0 +1,47 @@
+--## Extracting countries' codes, names, and region, indicators' codes and names, values of metrics within target range of years
+SELECT country.country_code, country.table_name, country.region, indicator.indicator_code, indicator.indicator_name, indicator.year, indicator.value
+
+FROM `bigquery-public-data.world_bank_wdi.country_summary` AS country
+
+--Joining indicators data table for values of metrics
+LEFT JOIN `bigquery-public-data.world_bank_wdi.indicators_data` AS indicator
+  ON country.country_code = indicator.country_code
+
+WHERE
+  --Filtering OUT unnecessary groups, result: 249
+  country.table_name NOT LIKE '%excluding high income%'
+  AND country.table_name NOT LIKE '%IDA%'
+  AND country.table_name NOT LIKE '%IBRD%'
+
+  --Use 1 of 3 filters depending on data availability
+  --1. Filtering LOCATION and REGION
+  --AND (country.income_group IS NOT NULL
+  --OR country.region IS NULL)
+
+  --2. Filtering LOCATION, result: 216
+  --  AND country.income_group IS NOT NULL
+
+  --3. Filtering REGION, result: 20
+    AND country.region IS NULL
+  --  AND country.table_name NOT LIKE '%dividend%'
+  --  AND country.table_name NOT LIKE '%income%'
+  
+  --Getting values from indicators
+  AND
+  (
+    indicator.indicator_code = 'SE.SEC.UNER.LO.ZS' --% of adolescents out of school
+    OR 
+    indicator.indicator_code = 'SE.PRM.UNER.ZS' --% of children out of school 
+    OR
+    indicator.indicator_code = 'SE.SEC.ENRL.TC.ZS' --pupil:teacher (secondary)
+    OR 
+    indicator.indicator_code = 'SE.PRM.ENRL.TC.ZS' --pupil:teacher (primary)
+    OR
+    indicator.indicator_code = 'SE.COM.DURS' --compulsory education (years)
+    OR
+    indicator.indicator_code = 'SE.ADT.1524.LT.ZS' --literacy rate of ages 15-24
+  ) 
+  
+  --Getting target range of years
+  AND
+  indicator.year BETWEEN 2011 AND 2020
