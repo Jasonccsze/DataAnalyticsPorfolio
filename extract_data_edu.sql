6f5813f567debcdf2f1e2bbfe8249b436e74e28e
Jasonccsze <89509616+Jasonccsze@users.noreply.github.com>
2022-06-13 12:42:09 +0800

    Update extract_data_edu.sql

--- extract_data_edu.sql
+++ extract_data_edu.sql
@@ -1,5 +1,5 @@
 --## Extracting countries' codes, names, and region, indicators' codes and names, values of metrics within target range of years
-SELECT country.country_code, country.table_name, country.region, indicator.indicator_code, indicator.indicator_name, indicator.year, indicator.value
+SELECT country.region, country.table_name, indicator.year, indicator.indicator_name, indicator.value
 
 FROM `bigquery-public-data.world_bank_wdi.country_summary` AS country
 
@@ -8,20 +8,20 @@ LEFT JOIN `bigquery-public-data.world_bank_wdi.indicators_data` AS indicator
   ON country.country_code = indicator.country_code
 
 WHERE
-  --Filtering OUT unnecessary groups, result: 249
+  --Filtering OUT unnecessary groups
   country.table_name NOT LIKE '%excluding high income%'
   AND country.table_name NOT LIKE '%IDA%'
   AND country.table_name NOT LIKE '%IBRD%'
 
   --Use 1 of 3 filters depending on data availability
-  --1. Filtering LOCATION, REGION, and INCOME GROUP
+  --1. Filtering LOCATION and REGION
   --AND (country.income_group IS NOT NULL
   --OR country.region IS NULL)
 
-  --2. Filtering LOCATION, result: 216
+  --2. Filtering LOCATION
   --  AND country.income_group IS NOT NULL
 
-  --3. Filtering REGION and INCOME GROUP, result: 20
+  --3. Filtering REGION and INCOME GROUP
     AND country.region IS NULL
   
   --Getting values from indicators
